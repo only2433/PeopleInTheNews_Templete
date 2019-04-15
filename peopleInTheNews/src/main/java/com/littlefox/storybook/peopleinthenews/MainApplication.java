bfc9d631422cf978a762a84166aaaca8dcabf840
only2433 <only2433@naver.com>
2019-04-15 15:02:55 +0900

    Initial commit

---
+++ peopleInTheNews/src/main/java/com/littlefox/storybook/peopleinthenews/MainApplication.java
@@ -0,0 +1,15 @@
+package com.littlefox.storybook.peopleinthenews;
+
+import android.app.Application;
+import android.util.DisplayMetrics;
+
+public class MainApplication extends Application{
+	
+	@Override
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+	
+	
+}
